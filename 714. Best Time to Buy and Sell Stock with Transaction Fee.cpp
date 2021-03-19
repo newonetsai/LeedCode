96658e25736fd9967b69d76a3ee6b614032fe21e
Newone Tsai <s100323028@gmail.com>
2021-03-19 09:12:00 +0800

    Update 714. Best Time to Buy and Sell Stock with Transaction Fee.cpp

--- 714. Best Time to Buy and Sell Stock with Transaction Fee.cpp
+++ 714. Best Time to Buy and Sell Stock with Transaction Fee.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices, int fee) {
         int cash=0,hold=-prices[0];
+        
         for(int day=2;day<(prices.size()+1);day++){
             cash=max(cash,hold+prices[day]-fee);//only cash: 1.original no stock 2.sell stock
             hold=max(hold,cash-prices[day]);//has stock: 1.original has stock 2.buy stock
