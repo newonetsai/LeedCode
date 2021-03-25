8110941fe1dcac1bb381a71d2af0615342b3d6bb
Newone Tsai <s100323028@gmail.com>
2021-03-25 23:42:33 +0800

    Create 387. First Unique Character in a String.cpp

---
+++ 387. First Unique Character in a String.cpp
@@ -0,0 +1,13 @@
+lass Solution {
+public:
+    int firstUniqChar(string s) {
+        unordered_map<char, int> map;
+        for (int c=0;c<s.length();c++) {
+            map[s[c]]++;
+        }
+        for (int i = 0; i < s.size(); i++) {
+            if (map[s[i]] == 1) return i;
+        }
+        return -1;
+    }
+};
