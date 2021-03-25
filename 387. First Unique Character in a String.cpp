89fa6200495a8d897b108a31fb9b669e42e13931
Newone Tsai <s100323028@gmail.com>
2021-03-25 23:43:45 +0800

    Update 387. First Unique Character in a String.cpp

--- 387. First Unique Character in a String.cpp
+++ 387. First Unique Character in a String.cpp
@@ -5,7 +5,7 @@ public:
         for (int c=0;c<s.length();c++) {
             map[s[c]]++;
         }
-        for (int i = 0; i < s.size(); i++) {
+        for (int i = 0; i < s.size(); i++) {//find first letter
             if (map[s[i]] == 1) return i;
         }
         return -1;
