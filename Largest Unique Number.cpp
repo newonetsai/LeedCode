af0b07cd3b5c2d4c8df985a4e7d33bc07fa2eb57
Newone Tsai <s100323028@gmail.com>
2021-04-04 21:46:34 +0800

    Create Largest Unique Number.cpp

---
+++ Largest Unique Number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int largestUniqueNumber(vector<int>& s) {
+        unordered_map<int, int> map;
+        // Store counts of each element
+        for (auto i : s) {
+            map[i]++;
+        }
+        
+        int result = -1;
+        for (auto [key, val] : map) {
+            // If map of the integer is 1, get maximum.
+            if (val == 1) {
+              result = result > key ? result : key;
+            }
+        }
+        return result;
+    }
+};
