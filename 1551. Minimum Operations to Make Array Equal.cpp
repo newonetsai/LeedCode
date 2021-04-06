30e3718117b00645a0d5043a8f880101f5225acb
Newone Tsai <s100323028@gmail.com>
2021-04-06 23:40:38 +0800

    Create 1551. Minimum Operations to Make Array Equal.cpp

---
+++ 1551. Minimum Operations to Make Array Equal.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int minOperations(int n) {
+        int Output=0, Average=(2*(n-1)+1+1)*n/2/n;
+        for(int i=0;i<n;i++){
+            if((2*i+1)>=Average)
+                break;
+            Output += (Average - (2*i+1));
+        }
+        return Output;
+    }
+};
