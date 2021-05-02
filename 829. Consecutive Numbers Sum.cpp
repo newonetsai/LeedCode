044a569972354a55d0f378cec444ab690e3c43f5
Newone Tsai <s100323028@gmail.com>
2021-05-02 23:13:50 +0800

    Create 829. Consecutive Numbers Sum.cpp

---
+++ 829. Consecutive Numbers Sum.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int consecutiveNumbersSum(int N) {
+        int Ans=1;
+        if(N<3) return Ans;
+        for(int i=2; i<=(sqrt(2 * N)); i++)
+        {
+            if(i%2==0)
+            {
+                if((N%i)==(i/2))
+                    Ans++;
+            }
+            else if((N%i)==0)
+                    Ans++;
+        }
+        return Ans;
+    }
+};
