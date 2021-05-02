ebf975d6a52196d571f3206c9965c824ce1c9ea8
Newone Tsai <s100323028@gmail.com>
2021-05-02 17:32:35 +0800

    Create 1041. Robot Bounded In Circle.cpp

---
+++ 1041. Robot Bounded In Circle.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    bool isRobotBounded(string instructions) {
+        int x = 0, y = 0, i = 0;
+        for (char & ins : instructions)
+            if (ins == 'R')
+                i++;
+            else if (ins == 'L')
+                i--;
+            else//ins == G
+            {
+                if(i%4 == 0)
+                    y++;
+                else if(abs(i)%4 == 1)
+                    x++;
+                else if(abs(i)%4 == 2)
+                    y--;
+                else 
+                    x--;
+            }
+        if (i%4)//direct not North
+            return true;
+        else if(x==0 && y==0)//No move
+            return true;
+        else 
+            return false;
+    }
+};
