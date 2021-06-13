532c31fa6fad14cc989a13ab6559248ef8192949
Newone Tsai <s100323028@gmail.com>
2021-06-14 00:32:10 +0800

    Update 1. Two Sum.cpp

--- 1. Two Sum.cpp
+++ 1. Two Sum.cpp
@@ -4,7 +4,7 @@ public:
         unordered_map<int, int> Map;
         for (int i = 0; i < nums.size(); i++) {
             if (Map.find(target - nums[i]) != Map.end()) //Map.end() not means last one
-            {
+            { 
                 return {Map[target - nums[i]], i};  
             }    
             Map[nums[i]] = i;    
